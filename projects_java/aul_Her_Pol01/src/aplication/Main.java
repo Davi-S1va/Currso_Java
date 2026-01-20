053dc0dfaf9d95343ff1e80a49e1ec0652835512
Davi Da Silva Nascimento <davibacana3@gmail.com>
2026-01-20 17:13:40 -0300

    feat: add salary calculation using inheritance and polymorphism
    feat: add polymorphic salary calculation for employee

---
+++ projects_java/aul_Her_Pol01/src/aplication/Main.java
@@ -0,0 +1,58 @@
+package aplication;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+import entities.OutsourcedEmployee;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc= new Scanner(System.in);
+		
+		List<Employee> list= new ArrayList<>();
+	
+		System.out.print("Entre com o número de fúncionarios:");
+		int n = sc.nextInt();
+		
+		for (int i=1; i<=n;i++) {
+			System.out.print("Fúncionario #"+i);
+			System.out.print("\nTercerizado (s/n)?");
+			char ch=sc.next().charAt(0);
+			System.out.println("Nome:");
+			String name=sc.nextLine();
+			sc.nextLine();
+
+			System.out.println("Horas:");
+			int hours =sc.nextInt();
+			System.out.print("Valor por hora: ");
+			double valuePerHour=sc.nextDouble();
+			
+			if(ch =='s') {
+				System.out.println("Adicional:");
+				double additionalCharge=sc.nextDouble();
+				Employee emp=new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge);
+				list.add(emp);
+			}
+			
+			else {
+				Employee emp=new Employee(name, hours, valuePerHour);
+				list.add(emp);
+			}
+			
+		}
+		System.out.println();
+		System.out.print("Pagamentos");
+		for(Employee emp: list) {
+			
+			System.out.println(emp.getName()+"- $"+ String.format("%.2f", emp.payment()));
+		}
+		
+		sc.close();
+	}
+
+}
